b261ead2b29504854f087006c11a39457c5543de
zmx2321@163.com <zmx2321@163.com>
2018-08-02 09:01:53 +0800

    rev

--- Java/demo/DynamicWebProject/courseServlet/src/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import entity.User;
-
-public interface UserDao {
-	//添加用户
-	public int addUser(User user);
-}
--- Java/demo/DynamicWebProject/courseServlet/src/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import entity.User;
-
-public interface UserService {
-	//用户注册
-	public boolean register(User user);
-}
