a08ff33bcbac67bda73cbe2c9f0dde393ca3d5fd
zmx2321@163.com <zmx2321@163.com>
2018-08-28 11:04:46 +0800

    ibt

---
+++ Java/__course__/_test_/test5/bookStore/WebContent/static/frontEndPage/js/index.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview dangdang page
+ * author zmx2321@163.com
+ * data 2018/8/14
+ */
+
+//工具方法
+var indexUtil = {
+
+};
+
+//tip
+var tip = function(){
+	
+}
+
+//窗口大小改变时加载
+$(window).on('resize', function(){
+    
+});
+
+//load
+$(function(){
+    tip();  //tip
+});
